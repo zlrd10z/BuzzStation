eb44028d8d324171432014e70ec649f3836098f5
Maurycy Lewandowski <64948473+zlrd10z@users.noreply.github.com>
2024-08-28 19:02:28 +0200

    Update potentiometer_test.py

--- buzzstation_software/hardware_tests/potentiometer_test.py
+++ buzzstation_software/hardware_tests/potentiometer_test.py
@@ -4,18 +4,18 @@ import busio
 import adafruit_ads1x15.ads1115 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
 
-# Inicjalizacja magistrali I2C
+# I2C bus initialization:
 i2c = busio.I2C(board.SCL, board.SDA)
 
-# Inicjalizacja ADS1115
+# ADS1115 initialization:
 ads = ADS.ADS1115(i2c, address = 0x4a)
 
-# Inicjalizacja kanału (np. AIN0)
+# Channels initialization:
 chan0 = AnalogIn(ads, ADS.P0)
 chan1 = AnalogIn(ads, ADS.P1)
 chan2 = AnalogIn(ads, ADS.P2)
 
-# Odczyt wartości z kanału
+# Reading values from the channels:
 while True:
 	print("----------------")
 	print("A0: " + str(chan0.voltage))

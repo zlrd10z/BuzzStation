be1fccaedc8804b0dcb09ae772b45e587cad1ace
Maurycy Lewandowski <64948473+zlrd10z@users.noreply.github.com>
2024-09-12 13:05:24 +0200

    Update arduino_software.ino

--- buzzstation_software/arduino_software/arduino_software.ino
+++ buzzstation_software/arduino_software/arduino_software.ino
@@ -1,16 +1,16 @@
 #include "pins_arduino.h"
 #include <SoftwareSerial.h>
-#include <stdio.h>
 
 #define LED_BUILTIN 13 
-unsigned char buffor[1001];
-int buffor_position;
+#define BUFFER_SIZE 1000
+
+unsigned char buffor[BUFFER_SIZE];
+int buffor_position = 0;
 unsigned char stop_byte = 224;
 unsigned char byte_midi_output_2 = 222;
 unsigned char byte_midi_output_3 = 223;
 
-// Midi output 3:
-SoftwareSerial softSerial(3,2); // Digital ports as 2 (RX) and 3 (TX)
+SoftwareSerial softSerial(3, 2); // Digital ports as 2 (RX) and 3 (TX)
 
 void setup() {
   Serial.begin(31250); // 31250 baudrate - MIDI baudrate
@@ -19,44 +19,52 @@ void setup() {
 }
 
 void loop() {
-  if (Serial.available()) {
-    if (Serial.available() > 0) {
-      receiveData();
-      processData();
-    }
+  if (Serial.available() > 0) {
+    receiveData();
+    processData();
   }
 }
 
-// Receive data:
-void receiveData(){
-  digitalWrite(LED_BUILTIN, HIGH); // Indicate that data is transfered via Serial
+// Receive data
+void receiveData() {
+  digitalWrite(LED_BUILTIN, HIGH); // Indicate that data is transferred via Serial
   buffor_position = 0;
 
-  while (Serial.available() > 0 && buffor_position < 1000) {
-    buffor[buffor_position++] = Serial.read();  
+  while (Serial.available() > 0 && buffor_position < BUFFER_SIZE) {
+    buffor[buffor_position++] = Serial.read();
+    delay(1); // Small delay to allow buffer to fill
   }
-  buffor[buffor_position] = stop_byte;
   digitalWrite(LED_BUILTIN, LOW);
 }
 
 // Process data from the buffer, then route the data to MIDI output 2 or output 3
-void processData(){
-  buffor_position = 0;
-
-  while (buffor[buffor_position] != stop_byte){
-    if(buffor[buffor_position] == byte_midi_output_2){
-      playNoteMIDI2(buffor[buffor_position + 1], buffor[buffor_position + 2], buffor[buffor_position + 3]);
-      buffor_position += 4;
+void processData() {
+  int index = 0;
+  while (index < buffor_position) {
+    if (buffor[index] == stop_byte) {
+      // Stop byte detected, end of message
+      break;
     }
-    else if (buffor[buffor_position] == byte_midi_output_3){
-      playNoteMIDI3(buffor[buffor_position + 1], buffor[buffor_position + 2], buffor[buffor_position + 3]);
-      buffor_position += 4;
+    if (buffor[index] == byte_midi_output_2) {
+      if (index + 3 < buffor_position) {
+        playNoteMIDI2(buffor[index + 1], buffor[index + 2], buffor[index + 3]);
+        index += 4;
+      } else {
+        // Incomplete message
+        break;
+      }
+    } else if (buffor[index] == byte_midi_output_3) {
+      if (index + 3 < buffor_position) {
+        playNoteMIDI3(buffor[index + 1], buffor[index + 2], buffor[index + 3]);
+        index += 4;
+      } else {
+        // Incomplete message
+        break;
+      }
+    } else {
+      index++;
     }
-
-    else{buffor_position++;}
-
   }
-
 }
 
 void playNoteMIDI2(int channel, int note, int velocity) {

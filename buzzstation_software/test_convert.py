ed0007336c66c1a848a95e7edffd612682c49493
zlrd10z <lordzlord10z@gmail.com>
2024-10-14 16:51:48 +0200

    Corrections

--- buzzstation_software/test_convert.py
+++
@@ -1,5 +0,0 @@
-from core import convert_audio_to_temp
-
-input_path = "/home/obojetnie/samples/fh_kick_lowly.wav"
-o_path = convert_audio_to_temp.convert_to_pygame_format(input_path)
-print(o_path)

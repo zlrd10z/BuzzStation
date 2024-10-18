254dca90101dbed17614aa9bde66902a0a691b39
zlrd10z <lordzlord10z@gmail.com>
2024-10-18 19:48:55 +0200

    Added midi_sender function which will work in another process, and send_to_midi_proc function, which will stay in main process and will comunicate with midi_sender.
    The idea is to create fifo queue, so many threads could send signals almost the same time, like sending notes signals and filter cutoff manipulations, and this signals will not mix up,

---
+++ buzzstation_software/core/midi_and_sync/midi_process.py
@@ -0,0 +1,34 @@
+from core.midi_and_sync import midi_output1
+from core.midi_and_sync import midi_output2and3
+
+# This function will works as another process
+def midi_sender(queue);
+    while True:
+        data = queue.get()
+        option = data[0]
+        midi_data = data[1]
+        match option:
+            ## Send signal all notes off to all channels on all outputs:
+            case option 0:
+                # Output 2 and 3: Arduino revice this byte, and then sendind proper all notes off signals to output 2 and 3
+                midi_output2and3.send_data_to_arduino(bytes([247])) 
+                # Output 1:
+                midi_output1.all_notes_off()
+            case option 1:
+                midi_output1.send_data(midi_data)
+            case option 2:
+                midi_output2and3.send_data_to_arduino(midi_data)
+
+# This function stays in main process and comunicates with function midi_process:
+def send_to_midi_proc(queue, option, data=None):
+    match option:
+        case 'all notes off':
+            data = (0, data) #None as tuple padding
+        case 'output 1':
+            data = (1, data)
+        case 'output 2 and 3':
+            data = (2, data)
+        case _:
+            #prevent locking
+            return None
+        queue.put(data)

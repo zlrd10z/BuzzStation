c7b99d6e3b60191927b0092672837fa6e28b7820
zlrd10z <lordzlord10z@gmail.com>
2024-10-19 18:28:01 +0200

    Sync-out corrected, now wokrs properly with volca keys.

--- buzzstation_software/core/midi_and_sync/sync.py
+++ buzzstation_software/core/midi_and_sync/sync.py
@@ -1,25 +1,16 @@
 import RPi.GPIO as GPIO
 import time
+import sys
 
 
 GPIO.setmode(GPIO.BCM)
 pin = 23
 GPIO.setup(pin, GPIO.OUT)
 
-def sync_out(pinBoolean):
+def sync_out():
     try:
-        if pinBoolean:
-            GPIO.output(pin, GPIO.HIGH)
-        else:
-            GPIO.output(pin, GPIO.LOW)
+        GPIO.output(pin, GPIO.HIGH)    
+        GPIO.output(pin, GPIO.LOW)
     except KeyboardInterrupt as exception:
         print(exception)
         GPIO.cleanup()
-
-if __name__ == "__main__":
-    # Tests
-    while True:
-        sync_out(True)
-        time.sleep(0.1)
-        sync_out(False)
-        time.sleep(0.1)

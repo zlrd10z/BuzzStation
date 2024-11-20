007bd5997df7afe2a987a6b5af15055bb7746727
zlrd10z <lordzlord10z@gmail.com>
2024-11-20 15:41:07 +0100

    Update pots filtering

--- buzzstation_software/core/potentiometers_operations.py
+++ buzzstation_software/core/potentiometers_operations.py
@@ -36,20 +36,22 @@ def pots_operations(data_for_thread):
     while True:
         pots_values = pots_avr.get_values()
         bpm_old_value = song_data.get_data('bpm')
-        bpm_new_value = int(potentiometer_values_transform.bpm_from_potentiometer2(pots_values[1]))
+        bpm_new_value = int(potentiometer_values_transform.bpm_from_potentiometer2(pots_values[2]))
         if bpm_new_value - bpm_old_value > 1 or bpm_old_value - bpm_new_value > 1:
             bpm = bpm_new_value
             time_between_quarter_notes = potentiometer_values_transform.count_time_per_quarter(bpm)
             song_data.put_data('bpm', bpm)
             song_data.put_data('time_between_quarter_notes', time_between_quarter_notes)
         swing_old_value = song_data.get_data('swing')
-        swing_new_value = int(potentiometer_values_transform.swing_from_potentiometer1(pots_values[2]))
+        swing_new_value = int(potentiometer_values_transform.swing_from_potentiometer1(pots_values[0]))
         if swing_new_value - swing_old_value > 1 or swing_old_value - swing_new_value > 1:
             swing = swing_new_value
             song_data.put_data('swing', swing)
         bvol_old_value = song_data.get_data('bvol')
-        bvol_new_value = int(potentiometer_values_transform.volume_from_potentiometer0(pots_values[0]))
+        bvol_new_value = int(potentiometer_values_transform.volume_from_potentiometer0(pots_values[1]))
         if bvol_new_value - bvol_old_value > 1 or bvol_old_value - bvol_new_value > 1:
+            if bvol < 0:
+                bvol_new_value = 0
             bvol = bvol_new_value
             song_data.put_data('bvol', bvol)
             vol_command = f"amixer set Speaker Playback Volume {bvol}%"

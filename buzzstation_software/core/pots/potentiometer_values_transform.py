4e9e61f7c8c3703049240e1eb393aa50923e0f99
zlrd10z <lordzlord10z@gmail.com>
2024-11-24 15:41:01 +0100

    Change BPM range

--- buzzstation_software/core/pots/potentiometer_values_transform.py
+++ buzzstation_software/core/pots/potentiometer_values_transform.py
@@ -9,7 +9,7 @@ def scale_transform(y_min, y_max, x):
     y += y_min
     return y
 
-bpm_from_potentiometer2 = lambda pot_value: scale_transform(y_min = 30, y_max = 260, x = pot_value)
+bpm_from_potentiometer2 = lambda pot_value: scale_transform(y_min = 1, y_max = 210, x = pot_value)
 swing_from_potentiometer1 = lambda pot_value: scale_transform(y_min = -52, y_max = 50, x = pot_value)
 volume_from_potentiometer0 = lambda pot_value: scale_transform(y_min = -1, y_max = 100, x = pot_value)
 count_time_per_quarter = lambda bpm_value: float(Decimal(60) / Decimal(bpm_value)/ 4) 
